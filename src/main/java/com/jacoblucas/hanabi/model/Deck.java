5b5e89f487fa882166b5eb67cb8b0f14ca02344e
Jacob Lucas <jacob.lucas@grab.com>
2017-04-17 22:46:30 -0700

    Comments on Deck

--- src/main/java/com/jacoblucas/hanabi/model/Deck.java
+++ src/main/java/com/jacoblucas/hanabi/model/Deck.java
@@ -8,16 +8,22 @@ import java.util.Queue;
 
 import static java.util.Collections.shuffle;
 
+// Deck represents the deck of cards in the game of Hanabi, throughout the course of the game.
+// As cards are dealt to players, the size of the deck decreases.
 public class Deck {
     @Getter(AccessLevel.PROTECTED)
     private Queue<Card> cards;
 
+    // Constructor for a new, shuffled deck. This will initialise to be the complete number of cards in the game,
+    // prior to being dealt out to players.
     public Deck() {
         LinkedList<Card> cards = newInOrderDeck();
         shuffle(cards);
         this.cards = cards;
     }
 
+    // Builds a new, in-order deck of cards for the game.
+    // In each deck there are five 1's, two 2's, two 3's, two 4's, and one 5.
     protected static LinkedList<Card> newInOrderDeck() {
         LinkedList<Card> cards = new LinkedList<>();
         for (Suit suit : Suit.values()) {
@@ -37,6 +43,7 @@ public class Deck {
         return cards;
     }
 
+    // Returns the number of cards remaining in the deck.
     public int size() {
         return cards.size();
     }
